e355ac441f34fa29f6efa7c8abbd70160220fbec
Victor Allen <vamuigua@gmail.com>
2016-08-19 08:50:41 +0300

    modified view

--- app/view.py
+++ app/view.py
@@ -1,23 +1,28 @@
-from flask import render_template, flash, redirect
-from app import app
+from flask import render_template
+import app
+
 
 @app.route('/')
 @app.route('/<user>')
 def home(user=None):
-    return render_template("user.html",user = user)
+    return render_template("user.html", title='Home', user=user)
+
 
 @app.route('/about')
 def about():
-    return render_template("about.html",about = about)
+    return render_template("about.html", title='About', about=about)
+
 
 @app.route('/contact')
 def contact():
-    return render_template("contact.html",contact = contact)
+    return render_template("contact.html", title='Contact Us', contact=contact)
+
 
 @app.route('/register')
 def register():
-    return render_template("register.html",register = register)
+    return render_template("register.html", title='Register', register=register)
+
 
 @app.route('/data')
 def data():
-    return render_template("data.html",data = data)
+    return render_template("data.html", title='Data', data=data)
