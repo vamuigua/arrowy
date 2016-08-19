6481a70ef5089f2c6fb5b308af54270de511fb4e
Victor Allen <vamuigua@gmail.com>
2016-08-19 08:50:25 +0300

    modified app

--- app.py
+++ app.py
@@ -1,5 +1,17 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, redirect
+
 app = Flask(__name__)
 
+
+@app.route('/')
+@app.route('/<name>')
+def index(name="TreeHouse"):
+    return render_template('register.html', name=name)
+
+
+@app.route('/save', methods=['POST'])
+def save():
+    return "<h1>Name saved!</h1>"
+
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(debug=True, port=8000, host='0.0.0.0')
